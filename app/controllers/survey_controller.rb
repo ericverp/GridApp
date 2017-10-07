9abd7e785c6bd0979089152ea8b4e9ddfdbebaef
3DPrintScanner <1988anthonythomas@gmail.com>
2017-10-07 10:51:24 +0100

    update to allow debugging of requests

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,2 +1,11 @@
 class SurveyController < ApplicationController
+	skip_before_action :verify_authenticity_token
+	def survey
+		#    this method calculates which product is the best given the survey
+		puts "message received"
+		puts request.raw_post 
+		render :json => {:first => "thisvalue", :req => request.raw_post}
+	end
+
+
 end
